70565af9273e5aa9fd6539d845492da82a1675d6
stefan <stefan.holmberg@systementor.se>
2025-04-14 13:40:44 +0200

    oop private public

--- classes.php
+++ classes.php
@@ -1,5 +1,112 @@
 <?php
 
+// 1. OOP - klasser - varför private och public
+//                   - constructor + exceptions   - VALID STATE
+// Javascript let a = { name: "Stefan", age: 50, car: "Renault" }
+// 2. Seed:a många random produkter
+//             why? paging (sida 1,2,3,4,5) - 10 produkter per sida
+//             why? paging (sida 1,2,3,4,5) - 3 produkter per sida
+ 
+// 3. Populära produkter : startsidan = 10 mest populära 
+
+
+// Vi ska bygga ett system som ska hantera personer och deras lön
+// Reglerna är: ska ha ett namn och en age
+// kan ha bil
+// om man är mer än 50 år så har man 300 kr i timmen
+// annars 200 kr i timmen
+// sen ska man kunna anropa calulcateSalary (hoursWorked) som ska ge en lön
+
+// När vi har  ett fält som vi vill skydda/säkerställa att det är i VALID STATE så ska vi 
+// använda oss av private variabler och getters/setters - 
+// public, private, protected (specialvariant av private - om man har ARV)
+
+class Employee{
+    public $name; // public = alla kan se och ändra
+    private $age; // private = endast synlig inifrån klassen
+    private $car; 
+
+    function getCar(){ // getter = hämta värde på en privat variabel
+        return $this->car;
+    }
+
+    function setCar($car){ // setter = sätta värde på en privat variabel
+        if(strlen($car) > 8){
+            throw new Exception("Bilen kan inte vara längre än 8 tecken!");
+        } else {
+            $this->car = $car;
+        }
+    }
+
+    function __construct($name, $age){ // constructor = initiera saker
+        $this->name = $name; // $this = den aktuella instansen av klassen
+        $this->setAge($age);
+        //$this->age = $age;
+    }
+
+   function calulcateSalary($hoursWorked){
+        if($this->age > 50){
+            return $hoursWorked * 300;
+        } else {
+            return $hoursWorked * 200;
+        }
+    }
+
+    function getAge(){ // getter = hämta värde på en privat variabel
+        return $this->age;
+    }
+    function setAge($age){ // setter = sätta värde på en privat variabel
+        if($age < 0){
+            throw new Exception("Ålder kan inte vara negativ!");
+        } else {
+            $this->age = $age;
+        }
+    }
+
+}
+$employee = new Employee("Stefan", -50); // $employee är en instans av klassen Employee
+echo $employee->getAge(); // -50
+
+//$employee->car = "Renault";
+$employee->setCar("Renault");
+echo $employee->getCar(); 
+$employee->setCar("");
+
+try{
+    $employee->setCar("LAMBORGHINI"); // max 8 tecken
+} catch (Exception $e) {
+    echo "Fel: " . $e->getMessage(); // Bilen kan inte vara längre än 8 tecken!
+}
+
+
+// nu är $employee i VALID STATE
+// $employee->name = "Stefan";
+// HELLRE ATT PROGRAMMET KRASCHAR ÄN ATT VI HAR EN INVALID STATE 
+$employee->setAge(-50); 
+// $employee->age = 50;
+echo $employee->name; // Stefan
+echo $employee->getAge(); // 50
+
+
+//$employee->age = -20; // -20 är inte VALID STATE
+
+// $employee är i INVALID STATE
+$salary = $employee->calulcateSalary(40); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SKAPA SEPARATA KLASSER FÖR VARJE FEL SOM KAN INTRÄFFA
 
 class NotEnoughBalanceException extends Exception { 
