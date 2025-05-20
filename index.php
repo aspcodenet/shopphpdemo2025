1fc6b058077772298598f3cf84923fbd6a64b4d0
stefan <stefan.holmberg@systementor.se>
2025-05-20 19:21:42 +0200

    logging

--- index.php
+++ index.php
@@ -13,84 +13,95 @@ $dotenv->load();
 // Pilar istf .
 // \ istf .
 
+$logger = require_once ("Utils/logging.php");
+
+
+$logger->info("Startign");
+
 // import * as dotenv from 'dotenv';
+try{
+
+    $router = new Router();
+    $router->addRoute('/', function () {
+        require_once( __DIR__ .'/Pages/index.php');
+    });
+    $router->addRoute('/category', function () {
+        require_once( __DIR__ .'/Pages/category.php');
+    });
+    $router->addRoute('/admin/products', function () {
+        require_once( __DIR__ .'/Pages/admin.php' );
+    });
+    $router->addRoute('/admin/edit', function () {
+        require_once( __DIR__ .'/Pages/edit.php');
+    });
+    $router->addRoute('/admin/new', function () {
+        require_once( __DIR__ .'/Pages/new.php');
+    });
+    $router->addRoute('/admin/delete', function () {
+        require_once( __DIR__ .'/Pages/delete.php');
+    });
+
+    $router->addRoute('/user/login', function () {
+        require_once( __DIR__ .'/Pages/users/login.php');
+    });
+    $router->addRoute('/user/logout', function () {
+        require_once( __DIR__ .'/Pages/users/logout.php');
+    });
+
+    $router->addRoute('/user/register', function () {
+        require_once( __DIR__ .'/Pages/users/register.php');
+    });
+
+    $router->addRoute('/user/registerThanks', function () {
+        require_once( __DIR__ .'/Pages/users/registerThanks.php');
+    });
+
+    $router->addRoute('/search', function () {
+        require_once( __DIR__ .'/Pages/search.php');
+    });
+
+    $router->addRoute('/api/addToCart', function () {
+        require_once( __DIR__ .'/ApiCode/cart.php');
+    });
+
+    $router->addRoute('/viewCart', function () {
+        require_once( __DIR__ .'/Pages/viewCart.php');
+    });
 
 
 
-$router = new Router();
-$router->addRoute('/', function () {
-    require_once( __DIR__ .'/Pages/index.php');
-});
-$router->addRoute('/category', function () {
-    require_once( __DIR__ .'/Pages/category.php');
-});
-$router->addRoute('/admin/products', function () {
-    require_once( __DIR__ .'/Pages/admin.php' );
-});
-$router->addRoute('/admin/edit', function () {
-    require_once( __DIR__ .'/Pages/edit.php');
-});
-$router->addRoute('/admin/new', function () {
-    require_once( __DIR__ .'/Pages/new.php');
-});
-$router->addRoute('/admin/delete', function () {
-    require_once( __DIR__ .'/Pages/delete.php');
-});
 
-$router->addRoute('/user/login', function () {
-    require_once( __DIR__ .'/Pages/users/login.php');
-});
-$router->addRoute('/user/logout', function () {
-    require_once( __DIR__ .'/Pages/users/logout.php');
-});
+    $router->addRoute('/addToCart', function () {
+        require_once( __DIR__ .'/Pages/addToCart.php');
+    });
 
-$router->addRoute('/user/register', function () {
-    require_once( __DIR__ .'/Pages/users/register.php');
-});
+    $router->addRoute('/removeFromCart', function () { // Betyder ta bort EN 
+        require_once( __DIR__ .'/Pages/removeFromCart.php');
+    });
 
-$router->addRoute('/user/registerThanks', function () {
-    require_once( __DIR__ .'/Pages/users/registerThanks.php');
-});
+    $router->addRoute('/checkout', function () { // Betyder ta bort EN 
+        require_once( __DIR__ .'/Pages/checkout.php');
+    });
 
-$router->addRoute('/search', function () {
-    require_once( __DIR__ .'/Pages/search.php');
-});
 
-$router->addRoute('/api/addToCart', function () {
-    require_once( __DIR__ .'/ApiCode/cart.php');
-});
+    $router->addRoute('/checkoutsuccess', function () { // Betyder ta bort EN 
+        require_once( __DIR__ .'/Pages/checkoutsuccess.php');
+    });
 
-$router->addRoute('/viewCart', function () {
-    require_once( __DIR__ .'/Pages/viewCart.php');
-});
+    $router->addRoute('/product', function () { // Betyder ta bort EN 
+        require_once( __DIR__ .'/Pages/viewproduct.php');
+    });
 
 
 
+    $router->dispatch();
 
-$router->addRoute('/addToCart', function () {
-    require_once( __DIR__ .'/Pages/addToCart.php');
-});
-
-$router->addRoute('/removeFromCart', function () { // Betyder ta bort EN 
-    require_once( __DIR__ .'/Pages/removeFromCart.php');
-});
-
-$router->addRoute('/checkout', function () { // Betyder ta bort EN 
-    require_once( __DIR__ .'/Pages/checkout.php');
-});
+}
+catch(Exception $ex){
+    $logger->error($ex->getTrace());
+}
 
 
-$router->addRoute('/checkoutsuccess', function () { // Betyder ta bort EN 
-    require_once( __DIR__ .'/Pages/checkoutsuccess.php');
-});
-
-$router->addRoute('/product', function () { // Betyder ta bort EN 
-    require_once( __DIR__ .'/Pages/viewproduct.php');
-});
-
-
-
-$router->dispatch();
 ?>
 
 
