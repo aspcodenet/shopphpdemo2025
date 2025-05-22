307348250d3cd0c39b3008afc2d47512409a8e53
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-05-22 09:57:19 +0200

    Update logging.php

--- Utils/logging.php
+++ Utils/logging.php
@@ -32,63 +32,6 @@ $fileHandler = new StreamHandler(__DIR__ . '/../logs/app.log', Logger::DEBUG);
 //$fileHandler->setFormatter(new JsonFormatter());
 $logger->pushHandler($fileHandler);
 
-// Custom handler for OpenObserve
-class OpenObserveHandler extends \Monolog\Handler\AbstractProcessingHandler
-{
-    private $client;
-    private $url;
-    private $auth;
-    private $organization;
-    private $stream;
-    private $token;
-    
-    public function __construct($level = Logger::DEBUG, $bubble = true)
-    {
-        parent::__construct($level, $bubble);
-        
-        $this->client = new Client();
-        $this->url = $_ENV['OPENOBSERVE_URL'];
-        $this->organization = $_ENV['OPENOBSERVE_ORGANIZATION'];
-        $this->stream = $_ENV['OPENOBSERVE_STREAM'];
-        $this->auth = base64_encode($_ENV['OPENOBSERVE_USERNAME'] . ':' . $_ENV['OPENOBSERVE_PASSWORD']);
-        //die($this->auth);
-    }
-    
-    // Monolog 3.x compatible write method
-    protected function write(\Monolog\LogRecord $record): void
-    {
-        $endpoint = "{$this->url}/api/{$this->organization}/{$this->stream}/_json";
-        
-        // Create a properly structured log object
-        $logData = [
-            'timestamp' => $record->datetime->format(\DateTimeInterface::ISO8601),
-            'level' => $record->level->name,
-            'message' => $record->message,
-            'channel' => $record->channel,
-            'context' => $record->context,
-            'extra' => $record->extra
-        ];
-        
-        try {
-            $this->client->post($endpoint, [
-                'verify' => false,
-                'headers' => [
-                    'Authorization' => 'Basic ' . $this->auth,
-                    'Content-Type' => 'application/json'
-                ],
-                'json' => $logData  // Send as a single object
-            ]);
-        } catch (\Exception $e) {
-            // Write to error log if sending to OpenObserve fails
-            die($e->getMessage());
-            //error_log('Failed to send log to OpenObserve: ' . $e->getMessage());
-        }
-    }
-}
-
-// Add OpenObserve handler
-$openObserveHandler = new OpenObserveHandler();
-$logger->pushHandler($openObserveHandler);
 
 return $logger;
 ?>
