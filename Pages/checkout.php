43e0096961d89582358402456d3f1d3ffd187f33
stefan <stefan.holmberg@systementor.se>
2025-06-09 14:38:29 +0200

    stripe

--- Pages/checkout.php
+++ Pages/checkout.php
@@ -1,24 +1,11 @@
 <?php
-
-// ONCE = en gång även om det blir cirkelreferenser
-#include_once("Models/Products.php") - OK även om filen inte finns
-
 require_once("vendor/autoload.php");
 
-use Stripe\LineItem;
-
-
-
 require_once("Models/Product.php");
-require_once("components/Footer.php");
 require_once("Models/Database.php");
 require_once("Models/Cart.php");
-require_once("components/SingleProduct.php");
 
 $dbContext = new Database();
-
-
-
 
 
 
@@ -36,8 +23,7 @@ $cart = new Cart($dbContext, $session_id, $userId);
 
 \Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET']);
 
-
-
+// Skapa en array med s.k LineItems som innehåller produkterna i kundvagnen
 $lineitems = [];
 foreach($cart->getItems() as $cartitem ){
     array_push($lineitems, [
@@ -53,15 +39,14 @@ foreach($cart->getItems() as $cartitem ){
     ]);
 }
 
-
 $checkout_session = \Stripe\Checkout\Session::create([
     "mode" => "payment",
     "success_url" => "http://localhost:8000/checkoutsuccess",
-    "cancel_url" => "http://localhost:8000/index.php",
+    "cancel_url" => "http://localhost:8000",
     "locale" => "auto",
     "line_items" => $lineitems
 ]);
 
-
 http_response_code(303);
 header("Location: " . $checkout_session->url);
+
